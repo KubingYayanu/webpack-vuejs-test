5ffa19ac86eed3891d302ccf8dfff0b0ad21d658
kubing <edkt973@gmail.com>
2019-10-28 16:47:06 +0800

    add vue loader and babel loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
+import hello from "./hello.vue";
 
 new Vue({
   el: "#app",
   mounted: function() {
-    console.log("Hello Webpack and Vue !");
-  }
+    console.log("Hello World");
+  },
+  components: { hello },
+  template: "<hello/>"
 });
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "babel-preset-vue"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]?[hash]"
+        }
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.js"
+    }
+  }
+};
